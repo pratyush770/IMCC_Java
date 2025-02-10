523484210a940d8b6fab8685df523d90f8bd4056
pratyush majumdar <pratyushmajumdar27@gmail.com>
2025-02-10 15:14:15 +0530

    final keyword done

---
+++ Java/src/pkgParameterPassing/MyCharacterUtility.java
@@ -0,0 +1,6 @@
+package pkgParameterPassing;
+
+//public class MyCharacterUtility extends CharacterUtility {  // final class cannot be overriden
+//	public final void countVowels(String str) {  // wont work since final method cannot be overriden
+//	}
+//}
